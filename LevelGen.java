65e959bb2124e6a7aaa4281e1a66012cd564eee9
Justin James RONALD <jronald@c3r2s2.wethinkcode.co.za>
2019-07-10 13:21:17 +0200

    LevelGen

---
+++ LevelGen.java
@@ -0,0 +1,14 @@
+package src;
+
+import src.*;
+import src.character.*;
+
+public class LevelGen{
+    private int[][] grid;
+
+    public int[][] getGrid(){
+        return this.grid;
+    }
+
+    public 
+}
